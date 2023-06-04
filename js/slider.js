bc5d177825695892ebcfc1ea8967c4c166837032
Ina Kurets <ina.kurets@yandex.by>
2023-06-04 19:01:58 +0300

    banner

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.banner__slider', {
+   wrapperClass: 'banner__wrapper',
+   slideClass:'banner__slide',
+    pagination: {
+      el: '.banner__pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.banner__next',
+      prevEl: '.banner__prev',
+    },
+  
+     });
